b28c3418443966fc7947a64119cede2ea45f8c16
Camille Aussignac <camaussignac@emi.u-bordeaux1.fr>
2018-09-24 10:25:10 +0200

    début du tp03

---
+++ Approche objet/tp/tp03/Circle.java
@@ -0,0 +1,7 @@
+import java.util.*;
+
+public class Circle {
+    int centerX;
+    int centerY;
+    int radius;
+}
---
+++ Approche objet/tp/tp03/SVG.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SVG {
+    private int width;
+    private int heigth;
+
+    public SVG(int width, int heigth) {
+        this.width = width;
+        this.heigth = heigth;
+    }
+}
