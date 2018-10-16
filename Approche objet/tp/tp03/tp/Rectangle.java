136149fb3b8b18534136998a501012ab6da902a5
H3lly <h3lly.h0lly@gmail.com>
2018-10-16 22:04:42 +0200

    rectangle now inherits from Shape

--- Approche objet/tp/tp03/tp/Rectangle.java
+++ Approche objet/tp/tp03/tp/Rectangle.java
@@ -1,13 +1,51 @@
 import java.util.*;
+import java.lang.Math;
 
-public class Rectangle {
-    Point a;
-    Point b;
+public class Rectangle extends Shape{
+  private final Point a;
+  private final Point b;
+  public Rectangle() {
+    Point p = new Point(0.0, 0.0);
+    a = p;
+    b = p;
+  }
+  public Rectangle(Point a, Point b) {
+    this.a = a;
+    this.b = b;
+  }
 
-    public Rectangle(Point a, Point b){
-      this.a = a;
-      this.b = b;
-    }
+  private double getWidth(){
+    return Math.abs(b.getX()-a.getX());
+  }
+
+  private double getHeight(){
+    return Math.abs(b.getY()-b.getY());
+  }
+  @Override
+  public double area() {
+    return this.getWidth()*this.getHeight();
+  }
+
+  @Override
+  public double perimeter() {
+    return 2 * (this.getWidth()+this.getHeight());
+  }
+
+  public String toString(){
+    StringBuilder sb = new StringBuilder();
+    sb.append("A ");
+    sb.append(this.a.toString());
+    sb.append(" B ");
+    sb.append(this.b.toString());
+    return sb.toString();
+  }
+  public static void main(String[] argc){
+    Point a = new Point(5.6, 8.5);
+    Point b = new Point(5.8, 9.9);
+    Rectangle r = new Rectangle(a, b);
+    System.out.println(r.toString());
+  }
+
 
 
 }
