cf970af72f895cc99ee1d9237cc6d304cf0de17c
H3lly <h3lly.h0lly@gmail.com>
2018-10-16 21:00:08 +0200

    ajout de Shape (td03)

--- Approche objet/tp/tp03/tp/Shape.java
+++ Approche objet/tp/tp03/tp/Shape.java
@@ -1,23 +1,38 @@
 import java.util.*;
 
 public class Shape {
-  double aera;
-  double perimeter;
+    double area;
+    double perimeter;
 
-  public Shape(double area, double perimeter){
-    //this.area = area;
-    //this.perimeter = perimeter;
-  }/*
+    public Shape(){
+      area = 0.0;
+      perimeter = 0.0;
+    }
 
-  public double getArea(){
-    return this.area;
-  }
+    public Shape(double area, double perimeter){
+      this.area = area;
+      this.perimeter = perimeter;
+    }
 
-  public double getPerimeter(){
-    return this.perimeter;
-  }
+    public double getArea(){
+      return this.area;
+    }
 
-  public String toString(){
-    return " ";
-  }*/
+    public double getPerimeter(){
+      return this.perimeter;
+    }
+
+    public String toString(){
+      StringBuilder sb = new StringBuilder();
+      sb.append("Area ");
+      sb.append(this.getArea());
+      sb.append(" Perimeter ");
+      sb.append(this.getPerimeter());
+      return sb.toString();
+    }
+
+    public static void main(String[] args){
+      Shape s = new Shape(5.0, 6.0);
+      System.out.println(s.toString());
+    }
 }
