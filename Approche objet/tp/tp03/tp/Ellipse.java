43329699a5e95952d43eefd3875baefe583335a5
H3lly <h3lly.h0lly@gmail.com>
2018-10-16 22:27:06 +0200

    all classes now inherits from Shape. TP finished

--- Approche objet/tp/tp03/tp/Ellipse.java
+++ Approche objet/tp/tp03/tp/Ellipse.java
@@ -1,31 +1,39 @@
 import java.util.*;
 
 public class Ellipse extends Circle {
-    public double radius2;
+  private double radius2;
 
-    /*public Ellipse(){
-      super();
-      this.radius2 = 0.0;
-    }
-    public Ellipse(Point center, double radius, double radius2){
-      super(center, radius);
-      this.radius2 = radius2;
-    }
+  public Ellipse(){
+    super();
+    this.radius2 = 0.0;
+  }
 
-    public double getRadius2(){
-      return this.radius2;
-    }
+  public Ellipse(double radius, Point center,double radius2){
+    super(radius, center);
+    this.radius2 = radius2;
+  }
 
-    public String toString(){
-      StringBuilder sb = new StringBuilder(1000);
-      sb.append(super.toString());
-      sb.append(" Radius2 ");
-      sb.append(this.getRadius2());
-      return sb.toString();
-    }
-    public static void main(String[] args){
-      Point p = new Point(5.0,-8.0);
-      Ellipse e = new Ellipse(p, 5.2, 53.0);
-      System.out.println(e.toString());
-    }*/
+  @Override
+  public double getArea(){
+    System.out.println("nedd to calculate area");
+    return 0.0;
+  }
+
+  public double getPerimeter(){
+    System.out.println("need to calculate perimeter");
+    return 0.0;
+  }
+
+  public String toString(){
+    StringBuilder sb = new StringBuilder(1000);
+    sb.append(super.toString());
+    sb.append(" Radius2 ");
+    sb.append(radius2);
+    return sb.toString();
+  }
+  public static void main(String[] args){
+    Point p = new Point(5.0,-8.0);
+    Ellipse e = new Ellipse(5.2, p, 53.0);
+    System.out.println(e.toString());
+  }
 }
